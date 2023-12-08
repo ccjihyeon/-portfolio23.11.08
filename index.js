dfe1920c380f7afd538d79c7f7fbc367d5a40fb4
ccjihyeon <130423140+ccjihyeon@users.noreply.github.com>
2023-12-08 15:12:26 +0900

    12.08
    일단 반응형 부터 만들기

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const elements = document.querySelectorAll('.hammenulist');
 
+const hambuttonClick = document.querySelector('.hambutton-cilck')
+
+
 function changeColor(element) {
     elements.forEach(function (el) {
         el.classList.remove('nav-active');
-        element.classList.add('nav-active');
     });
     element.classList.add('nav-active');
 }
 
-elements.forEach(function (element) {
-    element.addEventListener('click', function () {
+
+elements.forEach(function(element) {
+    element.addEventListener('click', ()=>{
         changeColor(element);
+        console.log(element);
     });
 });
+const button = document.querySelector('.hambutton');
+
+function closeMenu(){
+    button.classList.remove('active');
+    hambuttonClick.classList.remove('active')
+    // 메뉴 닫는부분 추가
+}
+
+function openMenu(){
+    button.classList.add('active');
+    hambuttonClick.classList.add('active')
+}
+
+button.addEventListener('click',()=>{
+    if(!button.classList.contains('active')) {
+        openMenu();
+    }else{
+        closeMenu();
+    }
+})
