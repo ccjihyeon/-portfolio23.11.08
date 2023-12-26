b4d670cfb05fe4d27101e53899f76986354add1f
ccjihyeon <130423140+ccjihyeon@users.noreply.github.com>
2023-12-26 15:02:26 +0900

    12.26
    인터렉션 추가하기ㅠ

---
+++ Intersection.js
@@ -0,0 +1,22 @@
+
+const historySection = document.querySelectorAll('.history-title-content');
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(handleIntersection, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+    });
+
+    observer.observe(historySection);
+});
