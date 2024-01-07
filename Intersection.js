78ddcd9a626d01e7f6c81127c82f5e1f979bdb5a
ccjihyeon <130423140+ccjihyeon@users.noreply.github.com>
2024-01-07 21:52:29 +0900

    1.7
    아이콘 나타내고 인터랙션 더 추가하기
    햄버튼 동작 추가하기

--- Intersection.js
+++ Intersection.js
@@ -22,24 +22,3 @@ document.addEventListener('DOMContentLoaded', function () {
         observer.observe(iterator);
     }
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    const clElement = document.querySelector('.cl');
-
-    function handleIntersection(entries, observer) {
-        entries.forEach(entry => {
-            const target = entry.target;
-            if (entry.isIntersecting) {
-                target.classList.add('visible');
-            }
-        });
-    }
-
-    const observer = new IntersectionObserver(handleIntersection, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.5
-    });
-
-    observer.observe(clElement);
-});
